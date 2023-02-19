9cf4baa9d2280e0d5a692c098f4d5f4c4dfb643f
masiucd <ciszekmarcell@gmail.com>
2023-02-19 19:11:16 +0100

    feat: Next canary version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css"
 
 import {Mulish as FontSerif, Space_Mono as FontMono} from "@next/font/google"
+import {Metadata} from "next"
 import {ReactNode} from "react"
 
 import {cn} from "@/lib/styles"
@@ -16,7 +17,46 @@ const fontMono = FontMono({
 	weight: ["400", "700"],
 })
 
-// const metaData: Metadata = {}
+export const metadata: Metadata = {
+	title: {
+		default: "Marcell Ciszek Druzynski",
+		template: "%s | Marcell Ciszek Druzynski",
+	},
+	description: "Software Developer, endurance freak and a nerd.",
+	openGraph: {
+		title: "Marcell Ciszek Druzynski",
+		description: "Software Developer, endurance freak and a nerd.",
+		url: "https://marcell-ciszek-druzynski-com-ltl6.vercel.app",
+		siteName: "Marcell Ciszek Druzynski",
+		images: [
+			{
+				url: "https://marcell-ciszek-druzynski-com-ltl6.vercel.app/og.jpg",
+				width: 1920,
+				height: 1080,
+			},
+		],
+		locale: "en-US",
+		type: "website",
+	},
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			"max-video-preview": -1,
+			"max-image-preview": "large",
+			"max-snippet": -1,
+		},
+	},
+	twitter: {
+		title: "Marcell Ciszek Druzynski",
+		card: "summary_large_image",
+	},
+	icons: {
+		shortcut: "/favicons/favicon.ico",
+	},
+}
 
 export default function RootLayout({children}: {children: ReactNode}) {
 	return (
