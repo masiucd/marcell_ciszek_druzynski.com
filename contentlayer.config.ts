74a8e8fe70526adc6e8ec181be97e7f7ecbcfca6
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2023-03-02 14:40:32 +0100

    fix: node as any

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -59,17 +59,17 @@ export default makeSource({
 				{
 					theme: "one-dark-pro",
 					// theme: "nord",
-					onVisitLine(node) {
+					onVisitLine(node: any) {
 						// Prevent lines from collapsing in `display: grid` mode, and allow empty
 						// lines to be copy/pasted
 						if (node.children.length === 0) {
 							node.children = [{type: "text", value: " "}]
 						}
 					},
-					onVisitHighlightedLine(node) {
+					onVisitHighlightedLine(node: any) {
 						node.properties.className.push("line--highlighted")
 					},
-					onVisitHighlightedWord(node) {
+					onVisitHighlightedWord(node: any) {
 						node.properties.className = ["word--highlighted"]
 					},
 				},
