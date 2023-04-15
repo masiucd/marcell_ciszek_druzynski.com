1501d7f87a4b1007cae031fa08d1f1e654a18680
masiucd <ciszekmarcell@gmail.com>
2023-04-15 14:25:38 +0200

    robots

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+	return {
+		rules: [
+			{
+				userAgent: "*",
+			},
+		],
+		sitemap: "https://www.marcellcd.com/sitemap.xml",
+		host: "https://www.marcellcd.com",
+	}
+}
