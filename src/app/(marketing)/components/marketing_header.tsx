a976b36bfaa88f2646b948a75bf2621726910c30
masiucd <ciszekmarcell@gmail.com>
2023-04-15 14:22:57 +0200

    Sitemap

--- src/app/(marketing)/components/marketing_header.tsx
+++ src/app/(marketing)/components/marketing_header.tsx
@@ -12,7 +12,9 @@ export default function MarketingHeader() {
 		<header className="sticky left-0 top-0 mb-10 bg-white dark:bg-black dark:text-white">
 			<div className=" mx-auto flex max-w-4xl items-center justify-between px-2 py-5">
 				<Link href="/">
-					<strong className="border-b-2 border-slate-900 text-xl">M.C.D</strong>
+					<strong className="border-b-2 border-slate-900 text-xl hover:opacity-60">
+						Marcell.C.D
+					</strong>
 				</Link>
 				<nav className="">
 					<ul className="flex gap-3">
