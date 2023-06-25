74b93a8324f5f3c6a25303290347ca0919585a6c
masiucd <ciszekmarcell@gmail.com>
2023-06-25 14:50:50 +0200

    New tooltip component

--- src/components/common/tooltip.tsx
+++ src/components/common/tooltip.tsx
@@ -1,5 +1,25 @@
-function Tooltip() {
-	return <div>Tooltip</div>;
+import {PlacesType, Tooltip as ReactTooltip} from "react-tooltip";
+
+type Props = {
+	place?: PlacesType;
+};
+
+function Tooltip({place = "top"}: Props) {
+	return (
+		<>
+			<a
+				data-tooltip-id="my-tooltip"
+				data-tooltip-content="Hello world!"
+				data-tooltip-place="top"
+			>
+				◕‿‿◕
+			</a>
+			<a data-tooltip-id="my-tooltip" data-tooltip-content="Hello to you too!">
+				◕‿‿◕
+			</a>
+			<ReactTooltip id="my-tooltip" place={place} />
+		</>
+	);
 }
 
 export default Tooltip;
