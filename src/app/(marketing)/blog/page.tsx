746a05a3c34749c8e967669ce14242eac4c616e8
masiucd <ciszekmarcell@gmail.com>
2023-07-09 09:21:27 +0200

    Group posts

--- src/app/(marketing)/blog/page.tsx
+++ src/app/(marketing)/blog/page.tsx
@@ -1,5 +1,5 @@
 import {allPosts} from "contentlayer/generated";
-import {compareDesc} from "date-fns";
+import {compareDesc, format, parseISO, startOfMonth} from "date-fns";
 import type {Metadata} from "next/types";
 
 import PageTitle from "@/components/common/page_title";
@@ -17,23 +17,51 @@ async function getPosts() {
 		return compareDesc(new Date(a.updated), new Date(b.updated));
 	});
 
-	return posts;
+	const postsPerMonth = posts.reduce((acc, post) => {
+		const month = startOfMonth(parseISO(post.updated));
+		const monthString = format(month, "yyyy-MM-dd");
+		if (!acc[monthString]) {
+			acc[monthString] = [];
+		}
+		acc[monthString].push(post);
+		return acc;
+	}, {} as Record<string, typeof posts>);
+
+	const groupedPosts = Object.keys(postsPerMonth).map((monthString) => ({
+		monthString,
+		posts: postsPerMonth[monthString],
+	}));
+
+	return groupedPosts;
 }
 
 async function BlogPage() {
 	const posts = await getPosts();
 	return (
-		<section className="flex max-w-2xl flex-1 flex-col  p-1">
+		<section className="flex max-w-2xl flex-1 flex-col p-1">
 			<PageTitle className="p-1">
 				<TypographyH1>Blog</TypographyH1>
 				<p className="pl-1">
-					Here where you can find blog posts about programming and tech.
+					Here where I write about my thoughts, ideas, and experiences, when it
+					comes to programming, and tech.
 				</p>
 			</PageTitle>
-			<ul className="flex max-w-xl flex-col gap-3 p-1">
+			<ul className="flex max-w-xl flex-col gap-3 space-y-5 p-1">
 				{posts.map((post) => (
-					<BlogItem key={post._id} post={post} />
+					<li key={post.monthString}>
+						<p className="text-2xl font-bold text-gray-500 dark:text-gray-400">
+							Posts from {format(parseISO(post.monthString), "MMM, yy")}{" "}
+						</p>
+						<ul className="ml-5">
+							{post.posts.map((post) => (
+								<BlogItem key={post._id} post={post} />
+							))}
+						</ul>
+					</li>
 				))}
+				{/* {posts.map((post) => (
+					<BlogItem key={post._id} post={post} />
+				))} */}
 			</ul>
 		</section>
 	);
--- src/app/(marketing)/components/theme-toggle.tsx
+++ src/app/(marketing)/components/theme-toggle.tsx
@@ -19,17 +19,15 @@ export function ThemeToggle() {
 	return (
 		<div className="z-40 flex h-6 w-6 items-center justify-center rounded-md">
 			<Tooltip text="ctr + t" variant={theme === "light" ? "dark" : "light"}>
-				<button onClick={handleTheme} className="flex">
-					<button
-						type="button"
-						className="flex h-7 w-7 items-center justify-center rounded-md "
-					>
-						{theme === "light" ? (
-							<Image src="/moon.svg" alt="sun" width="24" height="24" />
-						) : (
-							<Image src="/sun.svg" alt="sun" width="24" height="24" />
-						)}
-					</button>
+				<button
+					onClick={handleTheme}
+					className="flex h-7 w-7 items-center justify-center rounded-md"
+				>
+					{theme === "light" ? (
+						<Image src="/moon.svg" alt="sun" width="24" height="24" />
+					) : (
+						<Image src="/sun.svg" alt="sun" width="24" height="24" />
+					)}
 
 					<span className="sr-only">Toggle theme</span>
 				</button>
