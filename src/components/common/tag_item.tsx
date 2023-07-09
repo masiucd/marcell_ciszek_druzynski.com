2068e92eb1f5de79b8d4a82450bf1d15a2f16af8
masiucd <ciszekmarcell@gmail.com>
2023-07-09 08:27:03 +0200

    Update next

--- src/components/common/tag_item.tsx
+++ src/components/common/tag_item.tsx
@@ -9,7 +9,7 @@ export default function TagItem({tag, href}: Props) {
 	return (
 		<Link href={href}>
 			<span className="z-0 text-gray-900 drop-shadow-md hover:text-blue-500 hover:opacity-80 dark:text-gray-200">
-				&lambda;-{tag}
+				#{tag}
 			</span>
 		</Link>
 	);
