7f3d190b78a4d06dcbf5e0964bc9a7f471bb0944
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2023-07-20 16:31:47 +0200

    Button variants

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,30 +3,33 @@ import {type ButtonHTMLAttributes} from "react";
 
 import {cn} from "@/lib/styles";
 
-const buttonVariants = cva("rounded px-3 py-1", {
-	variants: {
-		variant: {
-			primary: "bg-blue-500 hover:bg-blue-600 text-white",
-			secondary: "bg-gray-500 hover:bg-gray-600 text-white",
-			tertiary: "bg-gray-200 hover:bg-gray-300 text-gray-900",
-			link: "bg-transparent hover:bg-gray-200 text-gray-900",
-			ghost: "bg-transparent hover:bg-gray-200 text-gray-900",
-			uniformed: " hover:bg-gray-300 text-gray-900",
+const buttonVariants = cva(
+	"rounded px-3 py-1 shadow text-gray-900 dark:text-white transition-colors duration-150 ease-in-out",
+	{
+		variants: {
+			variant: {
+				primary: "bg-blue-500 hover:bg-blue-600 hover:shadow-lg text-gray-100",
+				link: "bg-transparent shadow-none hover:underline  dark:text-gray-100 text-blue-500",
+				default: "bg-gray-300/20 hover:bg-gray-300/80",
+				subtle:
+					"bg-transparent hover:bg-gray-300/80 shadow-none dark:hover:bg-gray-100/80 dark:hover:text-gray-900",
+				"subtle-link": "hover:underline",
+			},
+			size: {
+				default: "h-10 px-3 py-2",
+				sm: "h-9 px-3",
+				md: "h-10  px-4",
+				lg: "h-11  px-8",
+				icon: "h-10 w-10",
+				reset: "h-10 w-10 p-0",
+			},
 		},
-		size: {
-			default: "h-10 px-4 py-2",
-			sm: "h-9 rounded-md px-3",
-			md: "h-10 rounded-md px-4",
-			lg: "h-11 rounded-md px-8",
-			icon: "h-10 w-10",
-			reset: "h-10 w-10 p-0",
+		defaultVariants: {
+			variant: "default",
+			size: "default",
 		},
-	},
-	defaultVariants: {
-		variant: "uniformed",
-		size: "md",
-	},
-});
+	}
+);
 
 export interface ButtonProps
 	extends ButtonHTMLAttributes<HTMLButtonElement>,
