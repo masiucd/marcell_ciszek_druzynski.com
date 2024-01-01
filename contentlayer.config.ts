0b6e659bac1dac6908a862507d3b067f9f69803d
masiucd <ciszekmarcell@gmail.com>
2024-01-01 12:58:25 +0100

    Remove unused import, new blog post

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -4,7 +4,6 @@ import {
 	makeSource,
 } from "contentlayer/source-files";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
-// import imageSize from "rehype-img-size";
 import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
