29d456593598f64596b865cc64bfe9d2eb7e9861
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2024-02-17 08:46:17 +0100

    Update max-width for article element

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -86,7 +86,7 @@ function Mdx({code, className}: Props) {
 			<ProgressLine />
 			<article
 				className={cn(
-					"prose prose-neutral dark:prose-invert mx-auto  prose-sm md:prose-base  ",
+					"prose prose-neutral dark:prose-invert mx-auto  prose-sm md:prose-base max-w-screen-lg",
 					className
 				)}
 			>
