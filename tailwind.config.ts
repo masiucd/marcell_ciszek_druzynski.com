752089f510f9fe1666fbc87a4bab2ddb5fd4b197
masiucd <ciszekmarcell@gmail.com>
2024-03-02 18:13:14 +0100

    Add next-mdx-remote and @tailwindcss/typography dependencies, update colors in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 import colors from "tailwindcss/colors";
 
 const config: Config = {
@@ -11,11 +11,24 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-				gray: colors.neutral,
-				primary: colors.blue,
-			},
+        gray: colors.zinc,
+        primary: colors.blue,
+        linen: {
+          "50": "hsl(30, 79%, 95%)",
+          "100": "hsl(29, 80%, 92%)",
+          "200": "hsl(28, 77%, 83%)",
+          "300": "hsl(27, 76%, 72%)",
+          "400": "hsl(24, 76%, 61%)",
+          "500": "hsl(21, 75%, 53%)",
+          "600": "hsl(17, 71%, 48%)",
+          "700": "hsl(14, 70%, 40%)",
+          "800": "hsl(11, 62%, 34%)",
+          "900": "hsl(12, 59%, 28%)",
+          "950": "hsl(10, 65%, 15%)",
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
 export default config;
