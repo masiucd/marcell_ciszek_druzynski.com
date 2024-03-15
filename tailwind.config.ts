c6d27de1dc9337f262e9cf447a08882870efd321
masiucd <ciszekmarcell@gmail.com>
2024-03-15 15:13:55 +0700

    Add background images and update colors in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,23 +10,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      background: {},
+      backgroundImage: {
+        "blog-title-bg-light": "url('/bg/curve-dark.svg')",
+        "blog-title-bg-dark": "url('/bg/curve-white.svg')",
+      },
       colors: {
         gray: colors.stone,
         primary: colors.blue,
-        linen: {
-          "50": "hsl(30, 79%, 95%)",
-          "100": "hsl(29, 80%, 92%)",
-          "200": "hsl(28, 77%, 83%)",
-          "300": "hsl(27, 76%, 72%)",
-          "400": "hsl(24, 76%, 61%)",
-          "500": "hsl(21, 75%, 53%)",
-          "600": "hsl(17, 71%, 48%)",
-          "700": "hsl(14, 70%, 40%)",
-          "800": "hsl(11, 62%, 34%)",
-          "900": "hsl(12, 59%, 28%)",
-          "950": "hsl(10, 65%, 15%)",
-        },
       },
     },
   },
