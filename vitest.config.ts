414729f892640baadb971391bb3d697844e8d9f1
masiucd <ciszekmarcell@gmail.com>
2024-06-08 20:02:37 +0200

    chore: Update vitest.config.ts for Jest configuration and npm dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import {defineConfig} from "vitest/config";
 
 export default defineConfig({
+  // @ts-ignore
   plugins: [react()],
   test: {
     environment: "jsdom",
