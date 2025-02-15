60f52fe4459c57594dfae875cfe31bf90771b0f4
marcell ciszek <ciszekmarcell@gmail.com>
2025-02-15 09:32:46 +0700

    feat: Add MDX support by updating Next.js configuration and dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
+import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
