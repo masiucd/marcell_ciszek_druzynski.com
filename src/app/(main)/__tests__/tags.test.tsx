5244b09c9280eb403167388d5534a10ce4b408c2
marcell ciszek <ciszekmarcell@gmail.com>
2025-02-15 07:50:14 +0700

    refactor: Remove unused files and configurations, add new PostCSS and ESLint configurations

--- src/app/(main)/__tests__/tags.test.tsx
+++
@@ -1,9 +0,0 @@
-import {render, screen} from "@testing-library/react";
-import {expect, test} from "vitest";
-
-import TagsPage from "../tags/page";
-
-test("TagsPage", () => {
-  render(<TagsPage />);
-  expect(screen.getByRole("heading", {level: 1, name: "Tags"})).toBeDefined();
-});
--- src/components/mcd.tsx
+++
@@ -1,9 +0,0 @@
-export default function Mcd() {
-  return (
-    <>
-      <span className="text-gray-600 dark:text-gray-500">Marcell</span>{" "}
-      <span className="text-gray-800 dark:text-gray-400">Ciszek</span>{" "}
-      <span className="text-gray-700 dark:text-gray-300">Druzynski</span>
-    </>
-  );
-}
