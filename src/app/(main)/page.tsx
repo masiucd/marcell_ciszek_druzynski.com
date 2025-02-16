ee03c657493f405859798bda2de4f622ef88b119
marcell ciszek <ciszekmarcell@gmail.com>
2025-02-16 13:11:26 +0700

    feat: Refactor Home page to use Card components for structured content display

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,13 @@
 import {PageWrapper} from "@/components/page-wrapper";
 import {H1, Lead} from "@/components/typography";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
 import siteData from "~/src/config/site-data";
 
 export default function Home() {
@@ -11,20 +19,74 @@ export default function Home() {
 				about web development, programming, and other tech-related topics.
 			</Lead>
 
-			<div className="border border-green-600 flex flex-1 justify-center items-center">
-				<div className="grid max-w-3xl grid-cols-12 gap-5 border border-blue-400 p-2">
-					<div className="col-span-8">
-						github activity Lorem ipsum dolor sit amet consectetur adipisicing
-						elit. Consequatur doloremque aliquam in dolores commodi numquam
-						iure, eius voluptatem rem obcaecati sed quis voluptates blanditiis
-						rerum necessitatibus nisi suscipit provident cumque.
+			<div className="flex flex-1 items-center justify-center border border-green-600">
+				<div className="grid max-w-3xl grid-cols-12 gap-2 border border-blue-400 p-2">
+					<div className="col-span-8 flex">
+						<Box
+							title="About me"
+							description="A little bit about me."
+							content="A little bit about me."
+							cardFooter="A little bit about me."
+						/>
 					</div>
-					<div className="col-span-4">dev wisdom</div>
-					<div className="col-span-6">now reading</div>
-					<div className="col-span-6">now learning</div>
-					<div className="col-span-12">recent posts</div>
+					<div className="col-span-4 flex">
+						<Box
+							title="Dev wisdom"
+							description="Dev quotes that inspire me."
+							content="Dev quotes that inspire me."
+							cardFooter="Dev quotes that inspire me."
+						/>
+					</div>
+					<div className="col-span-6 flex">
+						<Box
+							title="Now learning"
+							description="What I am currently learning."
+							content="What I am currently learning."
+							cardFooter="What I am currently learning."
+						/>
+					</div>
+					<div className="col-span-6 flex">
+						<Box
+							title="Now Reading"
+							description="Here you can find all the posts I have written."
+							content="Here you can find all the posts I have written."
+							cardFooter="Here you can find all the posts I have written."
+						/>
+					</div>
+					{/* <div className="col-span-12">recent posts</div> */}
+					<div className="col-span-12 flex">
+						<Box
+							title="Recent Posts"
+							description="Here you can find all the posts I have written."
+							content="Here you can find all the posts I have written."
+							cardFooter="Here you can find all the posts I have written."
+						/>
+					</div>
 				</div>
 			</div>
 		</PageWrapper>
+	);
+}
+
+// TODO rename the name and possible types of the props
+function Box({
+	title,
+	description,
+	content,
+	cardFooter,
+}: {title: string; description: string; content: string; cardFooter: string}) {
+	return (
+		<Card className="flex-1">
+			<CardHeader>
+				<CardTitle>{title}</CardTitle>
+				<CardDescription>{description}</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<p>{content}</p>
+			</CardContent>
+			<CardFooter>
+				<p>{cardFooter}</p>
+			</CardFooter>
+		</Card>
 	);
 }
