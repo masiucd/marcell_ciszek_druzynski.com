7ffcf0d0f90d948413064c00aba635387bb3baa4
marcell ciszek <ciszekmarcell@gmail.com>
2025-02-16 13:36:07 +0700

    feat: Update Home page layout and author introduction; refactor Box component usage

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,5 @@
 import {PageWrapper} from "@/components/page-wrapper";
-import {H1, Lead} from "@/components/typography";
+import {H1} from "@/components/typography";
 import {
 	Card,
 	CardContent,
@@ -13,23 +13,24 @@ import siteData from "~/src/config/site-data";
 export default function Home() {
 	return (
 		<PageWrapper>
-			<H1>Hi there! I'm {siteData.site.author}.</H1>
-			<Lead>
-				I'm a software engineer, web developer, and tech enthusiast. I write
-				about web development, programming, and other tech-related topics.
-			</Lead>
+			<div className="md:max-w-5xl">
+				<H1 className="text-pretty ">
+					Hi and welcome! I'm <AuthorName /> a software developer from
+					Gothenburg Sweden.
+				</H1>
+			</div>
 
 			<div className="flex flex-1 items-center justify-center border border-green-600">
 				<div className="grid max-w-3xl grid-cols-12 gap-2 border border-blue-400 p-2">
-					<div className="col-span-8 flex">
+					{/* <div className="col-span-8 flex">
 						<Box
 							title="About me"
 							description="A little bit about me."
 							content="A little bit about me."
 							cardFooter="A little bit about me."
 						/>
-					</div>
-					<div className="col-span-4 flex">
+					</div> */}
+					<div className="col-span-8 flex">
 						<Box
 							title="Dev wisdom"
 							description="Dev quotes that inspire me."
@@ -37,7 +38,7 @@ export default function Home() {
 							cardFooter="Dev quotes that inspire me."
 						/>
 					</div>
-					<div className="col-span-6 flex">
+					<div className="col-span-4 flex">
 						<Box
 							title="Now learning"
 							description="What I am currently learning."
@@ -54,7 +55,7 @@ export default function Home() {
 						/>
 					</div>
 					{/* <div className="col-span-12">recent posts</div> */}
-					<div className="col-span-12 flex">
+					<div className="col-span-6 flex">
 						<Box
 							title="Recent Posts"
 							description="Here you can find all the posts I have written."
@@ -88,5 +89,23 @@ function Box({
 				<p>{cardFooter}</p>
 			</CardFooter>
 		</Card>
+	);
+}
+
+function AuthorName() {
+	// let xs = siteData.site.author.split("").map((x) => (
+	// 	<span
+	// 		className="hover:scale-110 transform transition-transform duration-300 inline-block"
+	// 		key={x}
+	// 	>
+	// 		{x}{""}
+	// 	</span>
+	// ));
+
+	return (
+		<span className="after:-z-10 relative z-10 after:absolute after:bottom-1 after:left-0 after:block after:h-5 after:w-full after:rotate-1 after:rounded-md after:bg-blue-500/60 after:content-['']">
+			{siteData.site.author}
+			{/* {xs} */}
+		</span>
 	);
 }
