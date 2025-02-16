13f8ad752e582794c2d3e5a2348656d6e633da97
marcell ciszek <ciszekmarcell@gmail.com>
2025-02-16 14:28:00 +0700

    feat: Integrate sugar-high for enhanced code highlighting and add custom MDX components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,8 +1,17 @@
 import type {MDXComponents} from "mdx/types";
+import type {ComponentPropsWithoutRef} from "react";
+import {highlight} from "sugar-high";
+import {H2} from "./src/components/typography";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
 	return {
 		// Add custom components here
+		Foo: (props) => <H2 {...props}>Foo bar</H2>,
+		code: ({children, ...props}: ComponentPropsWithoutRef<"code">) => {
+			const codeHTML = highlight(children as string);
+			// biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+			return <code dangerouslySetInnerHTML={{__html: codeHTML}} {...props} />;
+		},
 		...components,
 	};
 }
