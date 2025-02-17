d40853b359f0125f00ada652df6fa66abf1af0ac
marcell ciszek <ciszekmarcell@gmail.com>
2025-02-17 13:50:06 +0700

    feat: Add data attribute for layout component to indicate installation status

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="en" data-lt-installed="true">
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
