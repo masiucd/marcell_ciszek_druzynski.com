da4fb9160d5177ea309e2d13516c5ca1810ed3d2
marcell ciszek <ciszekmarcell@gmail.com>
2025-02-18 15:47:30 +0700

    feat: Update PageWrapper to support blog post styling and add typography plugin

--- src/components/page-wrapper.tsx
+++ src/components/page-wrapper.tsx
@@ -4,17 +4,23 @@ import {cn} from "../lib/utils";
 type Props = {
 	className?: string;
 	as?: "div" | "section" | "article";
+	isBlogPost?: boolean;
 };
 
 export function PageWrapper({
 	children,
 	className,
 	as,
+	isBlogPost = false,
 }: PropsWithChildren<Props>) {
-	const Component = as ?? "section";
+	const Component = isBlogPost ? "article" : (as ?? "section");
 	return (
 		<Component
-			className={cn("mx-auto flex w-full max-w-6xl flex-1 flex-col", className)}
+			className={cn(
+				"mx-auto flex w-full flex-1 flex-col md:max-w-6xl",
+				isBlogPost && "prose dark:prose-dark md:max-w-3xl",
+				className,
+			)}
 		>
 			{children}
 		</Component>
