0554e29f01cb044b45a6ff4871272ad52ae142c5
marcell ciszek <ciszekmarcell@gmail.com>
2025-02-21 08:02:02 +0700

    feat: Refactor contact page by extracting Social and ContactForm components for improved readability and maintainability

--- src/app/(main)/contact/page.tsx
+++ src/app/(main)/contact/page.tsx
@@ -1,5 +1,5 @@
 import {PageWrapper} from "@/components/page-wrapper";
-import {H1, H2, H4, Lead, List} from "@/components/typography";
+import {H1, H2, H4, Lead} from "@/components/typography";
 import {Button} from "@/components/ui/button";
 import {
 	Card,
@@ -28,96 +28,80 @@ function ContactPage() {
 				<Lead>Get in touch with me!</Lead>
 			</div>
 
-			<div className="flex flex-col gap-20 md:max-w-5xl ">
-				<div className="">
-					<H2 className="mb-4">Social media, where you can find me </H2>
-					<ul className="flex justify-around gap-5">
-						{siteData.social.map((social) => (
-							<li
-								key={social.url}
-								className="flex size-42 items-center justify-center rounded-md shadow"
-							>
-								<a
-									href={social.url}
-									className="transition-opacity duration-150 hover:opacity-50"
-								>
-									{/* <Icon name={social.name} /> */}
-									<H4 className="capitalize">{social.name}</H4>
-								</a>
-							</li>
-						))}
-					</ul>
-				</div>
-				<div className="flex justify-between gap-20">
-					<Card>
-						<CardHeader>
-							<CardTitle>What I do</CardTitle>
-							<CardDescription>I write about:</CardDescription>
-						</CardHeader>
-						<CardContent>
-							<List>
-								<li>Web development</li>
-								<li>Programming</li>
-								<li>Other tech-related topics</li>
-								<li>UX</li>
-							</List>
-						</CardContent>
-						<CardFooter>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-							Voluptatem mollitia velit quidem consequatur et inventore,
-							distinctio nostrum nihil illum ab, hic explicabo dolores officiis
-							maiores alias, voluptates vel error deleniti?
-						</CardFooter>
-					</Card>
-
-					<Card className="w-full">
-						<CardHeader>
-							<CardTitle>Send a Message</CardTitle>
-							<CardDescription>
-								Fill out the form below to get in touch with me.
-							</CardDescription>
-						</CardHeader>
-						<form>
-							<CardContent className="space-y-4">
-								<div className="space-y-2">
-									<Label htmlFor="name">Name</Label>
-									<Input
-										id="name"
-										name="name"
-										placeholder="Your name"
-										required
-									/>
-								</div>
-								<div className="space-y-2">
-									<Label htmlFor="email">Email</Label>
-									<Input
-										id="email"
-										name="email"
-										type="email"
-										placeholder="Your email"
-										required
-									/>
-								</div>
-								<div className="space-y-2">
-									<Label htmlFor="message">Message</Label>
-									<Textarea
-										id="message"
-										name="message"
-										placeholder="Your message"
-										required
-									/>
-								</div>
-							</CardContent>
-							<CardFooter>
-								<Button type="submit" className="w-full">
-									Send Message
-								</Button>
-							</CardFooter>
-						</form>
-					</Card>
-				</div>
+			<div className="flex flex-col-reverse justify-between gap-20 md:flex-row">
+				<Social />
+				<ContactForm />
 			</div>
 		</PageWrapper>
+	);
+}
+
+function Social() {
+	return (
+		<div className="flex w-full flex-col">
+			<H2 className="mb-4">Social media, where you can find me </H2>
+			<ul className="flex flex-wrap gap-5">
+				{siteData.social.map((social) => (
+					<li
+						key={social.url}
+						className="flex size-28 items-center justify-center rounded-md shadow"
+					>
+						<a
+							href={social.url}
+							className="transition-opacity duration-150 hover:opacity-50"
+						>
+							{/* <Icon name={social.name} /> */}
+							<H4 className="capitalize">{social.name}</H4>
+						</a>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}
+
+function ContactForm() {
+	return (
+		<Card className="w-full">
+			<CardHeader>
+				<CardTitle>Send a Message</CardTitle>
+				<CardDescription>
+					Fill out the form below to get in touch with me.
+				</CardDescription>
+			</CardHeader>
+			<form>
+				<CardContent className="space-y-4">
+					<div className="space-y-2">
+						<Label htmlFor="name">Name</Label>
+						<Input id="name" name="name" placeholder="Your name" required />
+					</div>
+					<div className="space-y-2">
+						<Label htmlFor="email">Email</Label>
+						<Input
+							id="email"
+							name="email"
+							type="email"
+							placeholder="Your email"
+							required
+						/>
+					</div>
+					<div className="space-y-2">
+						<Label htmlFor="message">Message</Label>
+						<Textarea
+							id="message"
+							name="message"
+							placeholder="Your message"
+							required
+						/>
+					</div>
+				</CardContent>
+				<CardFooter>
+					<Button type="submit" className="w-full">
+						Send Message
+					</Button>
+				</CardFooter>
+			</form>
+		</Card>
 	);
 }
 
