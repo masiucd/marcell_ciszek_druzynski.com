8f777e1322a59e92cf0770069d2cfc216a190473
marcell ciszek <ciszekmarcell@gmail.com>
2025-02-22 21:39:46 +0700

    feat: Refactor homepage layout by introducing Welcome and Boxes components for improved structure and readability

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -14,108 +14,128 @@ import siteData from "~/src/config/site-data";
 import {getMetaDataFromBlogPosts} from "~/src/lib/meta-info";
 
 export default function Home() {
-	let lastPublishedPosts = getLastPublishedPosts();
 	return (
 		<PageWrapper>
 			<div className="md:max-w-5xl">
 				<H1 className="text-pretty lg:text-4xl">
-					<TextLoop
-						className="overflow-y-clip"
-						transition={{
-							type: "spring",
-							stiffness: 900,
-							damping: 80,
-							mass: 10,
-						}}
-						variants={{
-							initial: {
-								y: 20,
-								rotateX: 90,
-								opacity: 0,
-								filter: "blur(4px)",
-							},
-							animate: {
-								y: 0,
-								rotateX: 0,
-								opacity: 1,
-								filter: "blur(0px)",
-							},
-							exit: {
-								y: -20,
-								rotateX: -90,
-								opacity: 0,
-								filter: "blur(4px)",
-							},
-						}}
-					>
-						<span>Hi and welcome</span>
-						<span>สวัสดีและยินดีต้อนรับ</span>
-						<span>Witam i zapraszam</span>
-						<span>こんにちは、ようこそ</span>
-						<span>Привіт і ласкаво просимо</span>
-					</TextLoop>{" "}
-					<br />
-					I'm <AuthorName /> a software developer from Gothenburg Sweden.
+					<Welcome /> <br />
+					I'm{" "}
+					<span className="after:-z-10 relative z-10 after:absolute after:bottom-1 after:left-0 after:block after:h-5 after:w-full after:rotate-1 after:rounded-md after:bg-blue-500/60 after:content-['']">
+						{siteData.site.author}
+					</span>{" "}
+					a software developer from Gothenburg Sweden.
 				</H1>
 			</div>
 
 			{/* TODO we could harcode the data for now until we come with a better solution */}
 			{/* TODO glow effect! */}
-			<div className="flex flex-1 items-center justify-center border border-green-600">
-				<div className="grid max-w-3xl grid-cols-12 gap-2 border border-blue-400 p-2">
-					<div className="col-span-8 flex">
-						<Box
-							title="Dev wisdom"
-							description="Dev quotes for inspiration and motivation"
-						>
-							<P>
-								The best code is no code at all. <br />- Some smart person
-							</P>
-						</Box>
-					</div>
-					<div className="col-span-4 flex">
-						<Box
-							title="Now learning"
-							description="What I am currently learning."
-						>
-							<P>
-								Golang (Go) <br />
-								Next.js
-							</P>
-						</Box>
-					</div>
-					<div className="col-span-6 flex">
-						<Box
-							title="Now Reading"
-							description="Here you can find all the posts I have written."
-						>
-							<p>"Here you can find all the posts I have written."</p>
-						</Box>
-					</div>
-					{/* <div className="col-span-12">recent posts</div> */}
-					<div className="col-span-6 flex">
-						<Box
-							title="Recent Posts"
-							description="Here you can find all the posts I have written."
-						>
-							<ul>
-								{lastPublishedPosts.map(({frontMatter}) => (
-									<li key={frontMatter.slug}>
-										<Link href={`/blog/posts/${frontMatter.slug}`}>
-											{frontMatter.postTitle}
-										</Link>
-									</li>
-								))}
-							</ul>
-						</Box>
-					</div>
-				</div>
-			</div>
+			<Boxes />
 		</PageWrapper>
 	);
 }
 
-// TODO rename the name and possible types of the props
+function Welcome() {
+	return (
+		<TextLoop
+			className="overflow-y-clip"
+			transition={{
+				type: "spring",
+				stiffness: 900,
+				damping: 80,
+				mass: 10,
+			}}
+			variants={{
+				initial: {
+					y: 20,
+					rotateX: 90,
+					opacity: 0,
+					filter: "blur(4px)",
+				},
+				animate: {
+					y: 0,
+					rotateX: 0,
+					opacity: 1,
+					filter: "blur(0px)",
+				},
+				exit: {
+					y: -20,
+					rotateX: -90,
+					opacity: 0,
+					filter: "blur(4px)",
+				},
+			}}
+		>
+			<span>Hi and welcome</span>
+			<span>สวัสดีและยินดีต้อนรับ</span>
+			<span>Witam i zapraszam</span>
+			<span>こんにちは、ようこそ</span>
+			<span>Привіт і ласкаво просимо</span>
+		</TextLoop>
+	);
+}
+
+function Boxes() {
+	let lastPublishedPosts = getLastPublishedPosts();
+	return (
+		<div className="flex flex-1 items-center justify-center border border-green-600">
+			<div className="grid max-w-3xl grid-cols-12 gap-2 border border-blue-400 p-2">
+				<div className="col-span-8 flex">
+					<Box
+						title="Dev wisdom"
+						description="Dev quotes for inspiration and motivation"
+					>
+						<P>
+							The best code is no code at all. <br />- Some smart person
+						</P>
+					</Box>
+				</div>
+				<div className="col-span-4 flex">
+					<Box title="Now learning" description="What I am currently learning.">
+						<P>
+							Golang (Go) <br />
+							Next.js
+						</P>
+					</Box>
+				</div>
+				<div className="col-span-6 flex">
+					<Box
+						title="Now Reading"
+						description="Here you can find all the posts I have written."
+					>
+						<P>
+							<a
+								href="https://pragprog.com/titles/tpp20/the-pragmatic-programmer-20th-anniversary-edition/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								Pragmatic Programmer
+							</a>{" "}
+							<br />
+						</P>
+					</Box>
+				</div>
+				{/* <div className="col-span-12">recent posts</div> */}
+				<div className="col-span-6 flex">
+					<Box
+						title="Recent Posts"
+						description="Here you can find all the posts I have written."
+					>
+						<ul>
+							{lastPublishedPosts.map(({frontMatter}) => (
+								<li key={frontMatter.slug}>
+									<Link href={`/blog/posts/${frontMatter.slug}`}>
+										{frontMatter.postTitle}
+									</Link>
+								</li>
+							))}
+						</ul>
+					</Box>
+				</div>
+			</div>
+		</div>
+	);
+}
+
 function Box({
 	title,
 	description,
@@ -132,14 +152,6 @@ function Box({
 			</CardHeader>
 			<CardContent>{children}</CardContent>
 		</Card>
-	);
-}
-
-function AuthorName() {
-	return (
-		<span className="after:-z-10 relative z-10 after:absolute after:bottom-1 after:left-0 after:block after:h-5 after:w-full after:rotate-1 after:rounded-md after:bg-blue-500/60 after:content-['']">
-			{siteData.site.author}
-		</span>
 	);
 }
 
