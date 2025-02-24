dc302c8fcb324cdaa914964cf4d7f7efc868ea0c
marcell ciszek <ciszekmarcell@gmail.com>
2025-02-24 13:37:23 +0700

    feat: Add Tooltip component with provider, trigger, and content for enhanced UI interaction

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,6 +18,7 @@ const buttonVariants = cva(
 					"bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
 				ghost: "hover:bg-accent hover:text-accent-foreground",
 				link: "text-primary underline-offset-4 hover:underline",
+				empty: "m-0 p-0 outline-none ring-0",
 			},
 			size: {
 				default: "h-9 px-4 py-2 has-[>svg]:px-3",
@@ -44,7 +45,6 @@ function Button({
 		asChild?: boolean;
 	}) {
 	const Comp = asChild ? Slot : "button";
-
 	return (
 		<Comp
 			data-slot="button"
